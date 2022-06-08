8a62df50ae3d99b0916229ce6de321439a7ee7b3
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-08 13:15:58 -0700

    Add function to print 9 times table, starting from 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,9 +17,26 @@ for (column = 0; column <= 9; column++)
 {
 product = row *column;
 tens = product / 10;
+ones = product % 10;
+if (column == 0)
+{
+_putchar('0');
+}
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(ones + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
 _putchar(tens + '0');
 _putchar(ones + '0');
 }
 }
 _putchar('\n');
+}
 }
