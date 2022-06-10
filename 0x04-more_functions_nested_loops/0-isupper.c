4268c1c3b027540f563a8d5792c225117c882040
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 14:56:59 -0700

    fix betty doc

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _isupper - check if a variable is uppercase.
+ * @c: passed in alphabet
+ * Return: Either 1 or 0.
+ */
 int _isupper(int c)
 {
 if (c > 64 && c < 91)
