589fa080eae1970d9ad757ea01a3e41044205316
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 04:06:29 -0700

    corrected betty coding style

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,8 @@
 #define LARGEST 10000000000
 /**
  * main - main block
- * Description: Find and print the first 98 Fibonacci numbers starting with 1 and 2.
+ * Description: Find and print the first 98 Fibonacci numbers 
+ * starting with 1 and 2.
  * Numbers should be coma and space separated.
  * Return: 0
  */
