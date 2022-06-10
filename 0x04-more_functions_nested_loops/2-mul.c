c2dda3c9c57fe2547bfdbedcfe9ebd3223e42592
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 15:25:50 -0700

    add mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - check if a variable is uppercase.
+ * @a: first multiple
+ * @b: second multiple
+ * Return: int.
+ */
+int mul(int a, int b)
+{
+return (a*b);
+}
