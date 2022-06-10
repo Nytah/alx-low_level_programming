dd0fa9f76865ed91b3646ee9f4f206c0ac94b6d0
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 15:08:58 -0700

    add is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check if a variable is uppercase.
+ * @c: passed in alphabet
+ * Return: Either 1 or 0.
+ */
+int _isupper(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+return (0);
+}
