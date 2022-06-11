b3a87996ea621ff9d011e7710354fe938a2691ee
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 19:06:46 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - print numbers.
+ */
+void more_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("01234567891011121314");
+printf("\n");
+}
+}
+
