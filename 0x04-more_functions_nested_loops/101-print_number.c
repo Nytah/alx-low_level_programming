aa853d769b7751b43780a0717ce0b005743b242e
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 22:11:46 -0700

    advance 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,50 +1,38 @@
 #include "main.h"
 
 /**
- * print_number -  Entry point
- * @n: Entry variable
+ * print_number - function that prints an integer..
+ * @n: input value to check
+ * Return: nothing.
  */
+
 void print_number(int n)
 {
-	char d1, d2, d3, d4;
-	unsigned int num;
+	unsigned int num, num2;
+	int i;
+	int aux = 1;
 
-	if (n >= 0 && n <= 9)
+	if (n < 0)
 	{
-		d1 = n;
-		_putchar(d1 + '0');
+		n = n * -1;
+		_putchar('-');
 	}
-	if (n >=  10 && n <= 99)
+	num = n;
+	num2 = num;
+	if (num > 9)
 	{
-		d1 = n / 10;
-		d2 = n % 10;
-		_putchar(d1 + '0'), _putchar(d2 + '0');
+		while (num >= 10)
+		{
+			aux = aux * 10;
+			num = num / 10;
+		}
+		_putchar((num2 / aux) + '0');
+		aux = aux / 10;
+
+		for (i = aux; i >= 1; i = i / 10)
+			_putchar((num2 / i) % 10 + '0');
 	}
-	if (n >= 100 && n <= 999)
-	{
-		d1 = n / 100;
-		d2 = (n / 10) % 10;
-		d3 = n % 10;
-		_putchar(d1 + '0');
-		_putchar(d2 + '0');
-		_putchar(d3 + '0');
-	}
-	if (n >= 1000 && n <= 9999)
-	{
-		d1 = n / 1000;
-		d2 = (n / 100) % 10;
-		d3 = (n % 100) / 10;
-		d4 = n % 10;
-		_putchar(d1 + '0');
-		_putchar(d2 + '0');
-		_putchar(d3 + '0');
-		_putchar(d4 + '0');
-	}
-	if (n <= -10 && n >= -99)
-	{
-		num = n * (-1);
-		d1 = num / 10;
-		d2 = num % 10;
-		_putchar('-'), _putchar(d1 + '0'), _putchar(d2 + '0');
-	}
+	else
+		_putchar(num + '0');
 }
+
