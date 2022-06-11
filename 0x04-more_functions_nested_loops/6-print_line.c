cc90427c5afa5727329388dba6e9fad23b63c6a2
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 19:19:04 -0700

    add print line func

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - print line.
+ * @n: line to print
+ */
+void print_line(int n)
+{
+while (n > 0)
+{
+printf("_");
+n--;
+}
+printf("\n");
+}
+
+
