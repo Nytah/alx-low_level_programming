2030754ce9f835c49a9e93e33d1155592883a33a
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-10 20:18:13 -0700

    add print

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - print line.
+ * @size: line to print
+ */
+void print_square(int size)
+{
+int i;
+for (i = 0; i <= size; i++)
+{
+printf("#");
+}
+printf("\n");
+}
+
