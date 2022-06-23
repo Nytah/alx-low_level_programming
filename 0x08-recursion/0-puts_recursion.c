9ba38dfd4f8de37492ae2d0874ed487560c79344
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-06-23 00:17:49 -0700

    Add function that prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string
- * Return: pointer
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ *
+ * Return: void
  */
-
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-puts(*(s + 0));
-_puts_recursion((s + 1));
-}
-else
-{
-puts('\n');
 return;
 }
+_print_rev_recursion(s + 1);
+puts(*s);
 }
