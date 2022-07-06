d2a1b407f6dc59b36595e2295933d17a2c29c294
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-07-06 11:11:06 -0700

    Add function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
- * @name: the string name
- * @f: the printing function pointer
+ * @name: name to print
+ * @f: function to print with
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
-f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
