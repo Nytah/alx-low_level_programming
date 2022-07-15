6f03c178076b427eee938c78b309410af36712d5
Benita Onwubuya <onwubuyabenita@gmail.com>
2022-07-15 05:38:09 -0700

    Add function that adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list
- * @h: head of linked list
- * Return: number of elements
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of elements in the linked list
  */
-
 size_t list_len(const list_t *h)
 {
-	size_t size;
-	size = 0;
-	while (h != NULL)
-	{
-		size++;
-		h = h->next;
-	}
-	return (size);
+	const list_t *tmp;
+	unsigned int i;
+
+	tmp = h;
+	for (i = 0; tmp; i++)
+		tmp = tmp->next;
+	return (i);
 }
